573a199e328ab1b0f81dd9c90cdca0eb3bedff5b
安藤慎太郎 <shintaro@andoushintarounoMacBook-Pro.local>
2019-04-16 08:42:05 +0900

    FirstCommit

---
+++ src/main/java/com/example/demo/PostData.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="posts")
+public class PostData {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private long id;
+	
+	@Column
+	private String content;
+
+	@Column
+	private Timestamp postTime;	
+	
+	@Column
+	private String userName;
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Timestamp getPostTime() {
+		return postTime;
+	}
+
+	public void setPostTime(Timestamp postTime) {
+		this.postTime = postTime;
+	}
+	
+}
